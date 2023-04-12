6af4e36777ff767b58402f2ce662d67576460e45
freelancing-solutions <mobiusndou@gmail.com>
2023-04-12 11:47:54 +0200

    news api

---
+++ src/tasks/news_scraper.py
@@ -0,0 +1,25 @@
+
+
+
+
+async def scrape_news_yahoo(tickers: list[str]) -> list[dict[str, list[dict[str, str]]]]:
+    """
+        **scrape_news_yahoo**
+            scrapes financial  articles from yahoo finance
+
+    :param tickers:
+    :return: a list of dicts containing ticker symbol as key and a list of articles as values
+    """
+    pass
+
+
+
+async def alternate_news_sources(tickers: list[str]) -> list[dict[str, list[dict[str, str]]]]:
+    """
+        **alternate_news_sources**
+            scrapes finance & tech news from alternate sources
+
+    :param tickers:
+    :return: a list of dicts containing ticker symbol as key and a list of articles as values
+    """
+    pass
