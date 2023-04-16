25fef7af68aa4555362f6d99343af3812b533092
freelancing-solutions <mobiusndou@gmail.com>
2023-04-16 14:51:30 +0200

    Intergrated Caching into requests

--- src/tasks/__init__.py
+++ src/tasks/__init__.py
@@ -7,11 +7,30 @@ import asyncio
 import requests
 import feedparser
 import aiohttp
+from requests_cache import CachedSession
 
 from models import Exchange, Stock
 from src.models import Stock, RssArticle
 from src.config import confing_instance
 from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
+
+request_session = CachedSession('finance_news.cache', use_cache_dir=True,
+                                cache_control=False,
+                                # Use Cache-Control response headers for expiration, if available
+                                expire_after=timedelta(hours=3),
+                                # Otherwise expire responses after one day
+                                allowable_codes=[200, 400],
+                                # Cache 400 responses as a solemn reminder of your failures
+                                allowable_methods=['GET', 'POST'],
+                                # Cache whatever HTTP methods you want
+                                ignored_parameters=['api_key'],
+                                # Don't match this request param, and redact if from the cache
+                                match_headers=['Accept-Language'],
+                                # Cache a different response per language
+                                stale_if_error=True,
+                                # In case of request errors, use stale cache data if possible
+                                )
 
 
 async def get_exchange_tickers(exchange_code: str) -> list[Stock]:
@@ -25,19 +44,17 @@ async def get_exchange_tickers(exchange_code: str) -> list[Stock]:
     url: str = f'https://gateway.eod-stock-api.site/api/v1/stocks/exchange/code/{exchange_code}'
     params: dict = dict(api_key=confing_instance.EOD_STOCK_API_KEY)
 
-    with requests.Session() as session:
+    response = request_session.get(url=url, params=params)
+    response.raise_for_status()
 
-        response = session.get(url=url, params=params)
-        response.raise_for_status()
+    if response.headers.get('Content-Type') == 'application/json':
+        response_data: dict[str, str | bool | dict[str, str]] = response.json()
+        if response_data.get('status', False):
+            stocks_list: list[dict[str, str]] = response_data.get('payload')
 
-        if response.headers.get('Content-Type') == 'application/json':
-            response_data: dict[str, str | bool | dict[str, str]] = response.json()
-            if response_data.get('status', False):
-                stocks_list: list[dict[str, str]] = response_data.get('payload')
+            return [Stock(**stock) for stock in stocks_list]
 
-                return [Stock(**stock) for stock in stocks_list]
-
-        return []
+    return []
 
 
 async def get_exchange_lists() -> list[Exchange]:
@@ -49,17 +66,16 @@ async def get_exchange_lists() -> list[Exchange]:
     url: str = f'https://gateway.eod-stock-api.site/api/v1/exchanges'
     params: dict = dict(api_key=confing_instance.EOD_STOCK_API_KEY)
 
-    with requests.Session() as session:
-        response: requests.Response = session.get(url=url, params=params)
-        response.raise_for_status()
+    response: requests.Response = request_session.get(url=url, params=params)
+    response.raise_for_status()
 
-        if response.headers.get('Content-Type') == 'application/json':
-            response_data: dict[str, str | bool | dict[str, str]] = response.json()
-            if response_data.get('status', False):
-                exchange_list: list[dict[str, str]] = response_data.get('payload')
+    if response.headers.get('Content-Type') == 'application/json':
+        response_data: dict[str, str | bool | dict[str, str]] = response.json()
+        if response_data.get('status', False):
+            exchange_list: list[dict[str, str]] = response_data.get('payload')
 
-                return [Exchange(**exchange) for exchange in exchange_list]
-        return []
+            return [Exchange(**exchange) for exchange in exchange_list]
+    return []
 
 
 async def parse_google_feeds(rss_url: str) -> list[RssArticle]:
@@ -105,12 +121,13 @@ async def do_soup(html) -> tuple[str, str]:
 
 async def download_article(link: str, timeout: int, headers: dict[str, str]) -> tuple[str, str] | tuple[None, str]:
     """
-    Download the article from the link stored in news_sentiment.link,
-    then store the results in news_sentiment.article
+    **download_article**
+        Download the article from the link stored in news_sentiment.link,
+        then store the results in news_sentiment.article
     """
     try:
         async with aiohttp.ClientSession(headers=headers) as session:
-            async with session.get(link, timeout=timeout) as response:
+            async with session.get(url=link, timeout=timeout) as response:
                 response.raise_for_status()
                 text: str = await response.text()
                 return text
