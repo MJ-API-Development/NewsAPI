377cc3535c8a0c9fd9824ac9b1d9c3ba25d759ef
freelancing-solutions <mobiusndou@gmail.com>
2023-04-16 14:55:57 +0200

    Code Refactored

--- src/main/main.py
+++ src/main/main.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 
 from src.config import settings
 from src.tasks.news_scraper import scrape_news_yahoo, alternate_news_sources
+from src.tasks import can_run_task
 
 description = """ News API Scrapper"""
 
@@ -32,40 +33,6 @@ tasks_lookup = {
     'alternate_news_sources': alternate_news_sources,
 }
 
-
-async def convert_to_time(time_str: str) -> datetime.time:
-    """
-    Converts a string representing time to a datetime.time object.
-
-    :param time_str: A string representing time in the format HH:MM:SS.
-    :return: A datetime.time object representing the input time.
-    :raises ValueError: If the input time string is invalid.
-    """
-    try:
-        time_obj = datetime.datetime.strptime(time_str, '%H:%M').time()
-    except ValueError as e:
-        raise ValueError(f"Invalid time format: {e}")
-    return time_obj
-
-
-async def can_run_task(schedule_time: str, task_details) -> bool:
-    """
-    Returns True if the task can be executed based on the schedule time and task details.
-
-    :param schedule_time: A string representing the scheduled time in the format HH:MM:SS.
-    :param task_details: A dictionary containing details about the task, including a boolean field 'task_ran'.
-    :return: True if the task can be executed, False otherwise.
-    """
-    current_time = datetime.datetime.now().time()
-    schedule_time = await convert_to_time(schedule_time)
-
-    # Calculate the difference between the schedule time and current time in minutes
-    time_diff = abs(schedule_time.hour - current_time.hour) * 60 + abs(schedule_time.minute - current_time.minute)
-
-    # Check if the time difference is less than or equal to 10 minutes and the task has not already run
-    return time_diff <= 10 and not task_details.task_ran
-
-
 tickers_list = ['AAPL', 'AMZN', 'GOOGL', 'TSLA', 'FB', 'NVDA', 'NFLX', 'MSFT', 'JPM', 'V', 'BAC', 'WMT', 'JNJ', 'PG',
                 'KO', 'PEP', 'CSCO', 'INTC', 'ORCL', 'AMD']
 
@@ -90,4 +57,3 @@ async def scheduled_task():
 @app.on_event("startup")
 async def startup_event():
     asyncio.create_task(scheduled_task())
-
--- src/tasks/__init__.py
+++ src/tasks/__init__.py
@@ -133,3 +133,36 @@ async def download_article(link: str, timeout: int, headers: dict[str, str]) -> 
                 return text
     except (aiohttp.ClientError, asyncio.TimeoutError) as e:
         return None
+
+
+async def convert_to_time(time_str: str) -> datetime.time:
+    """
+    Converts a string representing time to a datetime.time object.
+
+    :param time_str: A string representing time in the format HH:MM:SS.
+    :return: A datetime.time object representing the input time.
+    :raises ValueError: If the input time string is invalid.
+    """
+    try:
+        time_obj = datetime.strptime(time_str, '%H:%M').time()
+    except ValueError as e:
+        raise ValueError(f"Invalid time format: {e}")
+    return time_obj
+
+
+async def can_run_task(schedule_time: str, task_details) -> bool:
+    """
+    Returns True if the task can be executed based on the schedule time and task details.
+
+    :param schedule_time: A string representing the scheduled time in the format HH:MM:SS.
+    :param task_details: A dictionary containing details about the task, including a boolean field 'task_ran'.
+    :return: True if the task can be executed, False otherwise.
+    """
+    current_time = datetime.now().time()
+    schedule_time = await convert_to_time(schedule_time)
+
+    # Calculate the difference between the schedule time and current time in minutes
+    time_diff = abs(schedule_time.hour - current_time.hour) * 60 + abs(schedule_time.minute - current_time.minute)
+
+    # Check if the time difference is less than or equal to 10 minutes and the task has not already run
+    return time_diff <= 10 and not task_details.task_ran
