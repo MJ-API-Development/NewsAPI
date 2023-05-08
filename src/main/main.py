76b07199fc6cfb5fa192530e55bf4e57c155d440
freelancing-solutions <mobiusndou@gmail.com>
2023-05-08 14:07:46 +0200

    added database connection

--- src/main/main.py
+++ src/main/main.py
@@ -69,8 +69,8 @@ async def scheduled_task() -> None:
                 # Mark task as completed by setting task_ran to True and then store back into scheduler
                 task_details.task_ran = True
                 scheduler_settings.schedule_times[schedule_time] = task_details
-                # Sleep for 10 minutes
-                await asyncio.sleep(600)
+                # Sleep for 1 hour minutes
+                await asyncio.sleep(600*6)
             # sleep one minute then run again
             await asyncio.sleep(60)
 
